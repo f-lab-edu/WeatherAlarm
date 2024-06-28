ae218692c5c2c16d74b9a0a934580f6fc70eb7ce
yswn1531 <yswn1531@gmail.com>
2024-06-28 13:35:47 +0900

    fix: app gradle 버전 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
-    id("com.google.dagger.hilt.android")
+    id("com.google.dagger.hilt.android") version "2.48"
     id("kotlin-kapt")
 }
 
