b3dd49da51431c51af50045d45c4fa77f92210a5
yswn1531 <yswn1531@gmail.com>
2024-06-30 16:51:40 +0900

    feat: AlarmRepositoryImpl 구현

---
+++ data/src/main/java/com/ysw/data/repository/AlarmRepositoryImpl.kt
@@ -0,0 +1,51 @@
+package com.ysw.data.repository
+
+import com.ysw.data.datasource.LocalAlarmDataSource
+import com.ysw.data.di.AlarmAppDispatchers
+import com.ysw.data.di.Dispatcher
+import com.ysw.data.entity.mapper.asDomain
+import com.ysw.data.entity.mapper.asEntity
+import com.ysw.domain.Alarm
+import com.ysw.domain.repository.AlarmRepository
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.withContext
+import java.time.LocalTime
+import javax.inject.Inject
+
+class AlarmRepositoryImpl @Inject constructor (
+    private val localDatasource: LocalAlarmDataSource,
+    @Dispatcher(AlarmAppDispatchers.IO) private val ioDispatcher: CoroutineDispatcher
+) : AlarmRepository {
+    override fun getAllAlarm(): Flow<List<Alarm>> {
+        return localDatasource.getAllAlarms().map { alarmEntityList ->
+            alarmEntityList.map { alarmEntity -> alarmEntity.asDomain() }
+        }.flowOn(ioDispatcher)
+    }
+
+    override suspend fun getAlarm(time: LocalTime): Alarm {
+        return withContext(ioDispatcher) {
+            localDatasource.getAlarm(time).asDomain()
+        }
+    }
+
+    override suspend fun insertAlarm(alarm: Alarm) {
+        return withContext(ioDispatcher) {
+            localDatasource.insertAlarm(alarm.asEntity())
+        }
+    }
+
+    override suspend fun deleteAlarm(time: LocalTime) {
+        return withContext(ioDispatcher) {
+            localDatasource.deleteAlarm(time)
+        }
+    }
+
+    override suspend fun turnOffAlarm(isOn: Boolean, time: LocalTime) {
+        return withContext(ioDispatcher) {
+            localDatasource.turnOffAlarm(isOn, time)
+        }
+    }
+}
