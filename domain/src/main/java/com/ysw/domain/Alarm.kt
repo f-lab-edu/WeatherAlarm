649ca80ae18292e8c32215c0c450f8c3e047c5b6
yswn1531 <yswn1531@gmail.com>
2024-06-30 16:52:22 +0900

    feat: Domain의 Alarm Data Class 구현 및 Mapper 구현

--- domain/src/main/java/com/ysw/domain/Alarm.kt
+++ domain/src/main/java/com/ysw/domain/Alarm.kt
@@ -1,12 +1,12 @@
-package com.ysw.presentation.compose
+package com.ysw.domain
 
 import android.net.Uri
 import java.time.LocalTime
 
-data class AlarmUiState(
+data class Alarm (
     val time : LocalTime = LocalTime.now(),
-    val alarmList : List<String> = emptyList(),
+    val alarmDayList : List<String> = emptyList(),
     val volume : Float = 0.0f,
     val isOn : Boolean = true,
     val musicListByWeather : Map<String, Uri> = emptyMap()
-    )
+)
