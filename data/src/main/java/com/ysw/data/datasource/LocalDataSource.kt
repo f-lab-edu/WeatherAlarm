d1aa488393727e01028683cb8cbd7a674c68eb84
yswn1531 <yswn1531@gmail.com>
2024-07-03 11:35:59 +0900

    rename: LocalData 부분에서의 onOffAlarm 이름 수정

--- data/src/main/java/com/ysw/data/datasource/LocalDataSource.kt
+++ data/src/main/java/com/ysw/data/datasource/LocalDataSource.kt
@@ -7,9 +7,9 @@ import java.time.LocalTime
 import javax.inject.Inject
 
 class LocalAlarmDataSource @Inject constructor (private val alarmDao: AlarmDao) {
-    fun getAllAlarms(): Flow<List<AlarmEntity>> = alarmDao.getAllAlarm()
+    fun getAllAlarms(): Flow<List<AlarmEntity>> = alarmDao.getAllAlarms()
     suspend fun getAlarm(time: LocalTime): AlarmEntity = alarmDao.getAlarm(time)
     suspend fun insertAlarm(alarm: AlarmEntity) = alarmDao.insertAlarm(alarm)
     suspend fun deleteAlarm(time: LocalTime) = alarmDao.deleteAlarm(time)
-    suspend fun onOffAlarm(isOn: Boolean, time: LocalTime) = alarmDao.onOffAlarm(isOn, time)
+    suspend fun setOnOffAlarm(isOn: Boolean, time: LocalTime) = alarmDao.setOnOffAlarm(isOn, time)
 }
