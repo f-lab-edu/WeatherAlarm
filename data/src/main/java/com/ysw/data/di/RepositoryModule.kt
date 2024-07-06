d9b4022a9dadf48bc607aa06180c092f95d139d8
yswn1531 <yswn1531@gmail.com>
2024-07-06 15:04:06 +0900

    refactor: 불필요한 import 제거

--- data/src/main/java/com/ysw/data/di/RepositoryModule.kt
+++ data/src/main/java/com/ysw/data/di/RepositoryModule.kt
@@ -4,7 +4,6 @@ import com.ysw.data.repository.AlarmRepositoryImpl
 import com.ysw.domain.repository.AlarmRepository
 import dagger.Binds
 import dagger.Module
-import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
@@ -12,6 +11,7 @@ import javax.inject.Singleton
 @Module
 @InstallIn(SingletonComponent::class)
 abstract class RepositoryModule {
+
     @Binds
     @Singleton
     abstract fun bindsAlarmRepository(alarmRepositoryImpl: AlarmRepositoryImpl): AlarmRepository
