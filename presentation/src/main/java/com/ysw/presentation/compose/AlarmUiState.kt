fe4e05f7e1fa77dbcf9c3780c321116a69efeb3b
yswn1531 <yswn1531@gmail.com>
2024-07-18 16:53:50 +0900

    refactor: UI명 UIState로 변경

--- presentation/src/main/java/com/ysw/presentation/compose/AlarmUiState.kt
+++ presentation/src/main/java/com/ysw/presentation/compose/AlarmUiState.kt
@@ -3,14 +3,14 @@ package com.ysw.presentation.compose
 import android.net.Uri
 import java.time.LocalTime
 
-data class AlarmListUi(
+data class AlarmListUiState(
     val id: Int = 0,
     val time: LocalTime = LocalTime.now(),
     val alarmList: List<String> = emptyList(),
     val isOn: Boolean = false,
 )
 
-data class AlarmSettingUi(
+data class AlarmSettingUiState(
     val id: Int = 0,
     val time: LocalTime = LocalTime.now(),
     val alarmList: List<String> = emptyList(),
